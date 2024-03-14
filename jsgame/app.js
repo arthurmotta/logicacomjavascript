5e21f2f0445b5b56ca46dd307ea4d86af9baa723
Arthur Almeida <arthuralmeidasocial@gmail.com>
2024-03-14 20:12:38 -0300

    atualizando o marcador na string

--- jsgame/app.js
+++ jsgame/app.js
@@ -1,11 +1,11 @@
 alert('Boas vindas ao jogo de número secreto');
 
-let numeroSecreto = 29;
+let numeroSecreto = 9;
 
 let chute = prompt('Escolha um número entre 1 e 30');
 
 if (numeroSecreto == chute){
-    alert('Você acertou!');
+    alert(`Você descobriu o número secreto! ${numeroSecreto}`);
 }else{
     alert('Você errou');
 }
