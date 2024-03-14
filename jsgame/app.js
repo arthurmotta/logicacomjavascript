c60ca97ebcb7852cd18975a0b613c6b53ed828e8
Arthur Almeida <arthuralmeidasocial@gmail.com>
2024-03-14 19:42:15 -0300

    trabalhando com alert e prompt e criando if

---
+++ jsgame/app.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao jogo de número secreto');
+
+let numeroSecreto = 29;
+
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (numeroSecreto == chute){
+    alert('Você acertou!');
+}else{
+    alert('Você errou');
+}
