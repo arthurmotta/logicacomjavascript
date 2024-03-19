4520fe6af4c9408672759da957ff556110883861
Arthur Almeida <arthuralmeidasocial@gmail.com>
2024-03-18 21:05:21 -0300

    funções

--- jsgame2/js-curso-2-aula_-/app.js
+++ jsgame2/js-curso-2-aula_-/app.js
@@ -1,9 +1,49 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+exibirMensagemInicial();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
 
 function verificarChute(){
-    console.log('Botao clicado');
+    let chute = document.querySelector('input').value; 
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1','Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function limparCampo() {
+    document.querySelector('input').value = '';
+}
+
+function gerarNumeroAleatorio() {
+    return Math.floor(Math.random() * 10 + 1);
 }
