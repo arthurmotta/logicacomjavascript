9e52ccc9f5e4d761460f5ca26b01c7dc7c90ca29
Arthur Almeida <arthuralmeidasocial@gmail.com>
2024-04-08 19:44:05 -0300

    verificando repetição dos números sorteados dentro do array

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -2,6 +2,17 @@ function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+
+    for(let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        while(sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+    }
 }
 
 function obterNumeroAleatorio(min, max) {
