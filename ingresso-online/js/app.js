a7a3386894c9ebb6c3669749bc2b0f2fea5de5d6
Arthur Almeida <arthuralmeidasocial@gmail.com>
2024-04-09 20:14:46 -0300

    recuperando os inputs

---
+++ ingresso-online/js/app.js
@@ -0,0 +1,8 @@
+function comprar() {
+    let tipoDeIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = document.getElementById('qtd').value;
+
+    let quantidadePista = document.getElementById('qtd-pista').textContent;
+    let quantidadeSuperior = document.getElementById('qtd-superior').textContent;
+    let quantidadeInferior = document.getElementById('qtd-inferior').textContent;
+}
