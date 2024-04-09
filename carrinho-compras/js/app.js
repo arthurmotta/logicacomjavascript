c92bdd977ce4e6f9473d5186191a3132e757e436
Arthur Almeida <arthuralmeidasocial@gmail.com>
2024-04-09 13:34:49 -0300

    setando função limpar

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,7 +1,13 @@
+let carrinho = [];
+
 function adicionar() {
     let quantidade = document.getElementById('quantidade').value;
+    let produto = document.getElementById('produto').value;
 }
 
 function limpar() {
     document.getElementById('quantidade').value = '';
+    document.getElementById('lista-produtos').innerText = '';
+    document.getElementById('valor-total').innerText = 'R$0';
+    carrinho = [];
 }
