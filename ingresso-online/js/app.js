3db270aaa30bcfb89f410b7dea4debc48eaa8110
Arthur Almeida <arthuralmeidasocial@gmail.com>
2024-04-09 21:16:32 -0300

    atualizando quantidade disponível e fazendo verificações

--- ingresso-online/js/app.js
+++ ingresso-online/js/app.js
@@ -1,8 +1,31 @@
+let quantidadePista = parseInt(document.getElementById('qtd-pista').textContent);
+let quantidadeSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+let quantidadeInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+
 function comprar() {
     let tipoDeIngresso = document.getElementById('tipo-ingresso').value;
-    let quantidade = document.getElementById('qtd').value;
+    let quantidade = parseInt(document.getElementById('qtd').value);
 
-    let quantidadePista = document.getElementById('qtd-pista').textContent;
-    let quantidadeSuperior = document.getElementById('qtd-superior').textContent;
-    let quantidadeInferior = document.getElementById('qtd-inferior').textContent;
+    if(tipoDeIngresso == 'pista') {
+        if(quantidade > quantidadePista) {
+            alert('Quantidade de ingressos indisponível!');
+            return;
+        }
+        quantidadePista = quantidadePista - quantidade;
+        document.getElementById('qtd-pista').innerText = `${quantidadePista}`;
+    } else if (tipoDeIngresso == 'superior') {
+        if(quantidade > quantidadeSuperior) {
+            alert('Quantidade de ingressos indisponível!');
+            return;
+        }
+        quantidadeSuperior = quantidadeSuperior - quantidade;
+        document.getElementById('qtd-superior').innerText = `${quantidadeSuperior}`;
+    } else {
+        if(quantidade > quantidadeInferior) {
+            alert('Quantidade de ingressos indisponível!');
+            return;
+        }
+        quantidadeInferior = quantidadeInferior - quantidade;
+        document.getElementById('qtd-inferior').innerText = `${quantidadeInferior}`;
+    }
 }
